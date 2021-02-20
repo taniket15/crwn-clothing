0dba529b3f0643cf6e5c5563b3db3f79ad836a25
Taniket Mehra <taniket.mehra@joshtechnologygroup.com>
2021-02-21 04:10:00 +0530

    updated app.scss

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import "./App.css";
+import "./App.scss";
 import Header from "./components/header";
 import HomePage from "./pages/homepage";
 import ShopPage from "./pages/shop";
